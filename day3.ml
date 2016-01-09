48af21b77766093b506d1e12a644d2d84e40c979
Kurtis Seebaldt <kseebaldt@gmail.com>
2016-01-08 17:14:39 -0800

    Day 3 part II

--- day3.ml
+++ day3.ml
@@ -18,12 +18,30 @@ let next_house (x, y) c = match c with
   | _ -> (x, y)
 ;;
 
-let day3 string =
-  let (s, h) = Enum.fold (fun (set, house) c ->
-      let next = next_house house c in
-      ((HouseSet.add next set), next)) (HouseSet.empty, (0,0)) (String.enum string)
-  in
-  HouseSet.cardinal s
+let shift item l = match l with
+  | [] -> [item]
+  | h :: t -> t @ [item]
 ;;
-  
 
+let rec repeat n item = match n with
+  | 0 -> []
+  | n -> item :: (repeat (n-1) item)
+;;
+
+let visit n string =
+  let start = repeat n (HouseSet.add (0,0) HouseSet.empty, (0,0)) in
+  let final = Enum.fold (fun l c ->
+      let (set, house) = List.hd l in
+      let next = next_house house c in
+      shift ((HouseSet.add next set), next) l) start (String.enum string) in
+  let set = List.fold_left (fun acc (set, _) -> HouseSet.union acc set) HouseSet.empty final in
+  HouseSet.cardinal set
+;;
+
+let day3 string =
+  visit 1 string
+;;
+
+let day3_2 string =
+  visit 2 string
+;;
