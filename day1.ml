2d2f95694f5944d980356a0519467c7ed88a6fa6
Kurtis Seebaldt <kseebaldt@gmail.com>
2016-01-09 14:26:33 -0800

    Use Core lib for Day 1

--- day1.ml
+++ day1.ml
@@ -1,4 +1,4 @@
-open Batteries;;
+open Core.Std;;
 
 let dir = function
   | '(' -> 1
@@ -7,16 +7,27 @@ let dir = function
 ;;
 
 let day1 s = 
-  String.fold_left (fun floor c -> floor + (dir c)) 0 s
+  String.fold ~init:0 ~f:(fun floor c -> floor + (dir c)) s
 ;;
 
 let day1_2 s =
   let rec day1_2 = function
     | ("", _, pos) -> pos
     | (_, (-1), pos) -> pos
-    | (str, floor, pos) -> let c = String.get str 0 in
+    | (str, floor, pos) -> 
+      let c = String.get str 0 in
       let rest = (String.sub str 1 ((String.length str) - 1)) in
       let next_floor = floor + (dir c) in
       day1_2 (rest, next_floor, pos + 1) in
   day1_2 (s, 0, 0)
+;;
 
+let read_lines filename = In_channel.with_file filename ~f:(fun file ->
+  In_channel.input_lines file)
+;;
+
+let run filename =
+  let input = List.hd_exn (read_lines filename) in
+  Printf.printf "day1 : %d\n" (day1 input);
+  Printf.printf "day1_2 : %d\n" (day1_2 input)
+;;
--- day1_run.ml
+++ day1_run.ml
@@ -1,9 +1,6 @@
 open Day1;;
-open Batteries;;
+open Core.Std;;
 
-let () =
-  let input = Option.get (Enum.peek (File.lines_of "input1.txt")) in
-  Printf.printf "day1 : %d\n" (day1 input);
-  Printf.printf "day1_2 : %d\n" (day1_2 input)
+let () = run "input1.txt"
 ;;
 
